6d341b1072d7279e98862c00f99e182f1eec8ad4
Mike Miller <mike@ammoseek.com>
2019-10-23 16:29:03 -0400

    Uploaded index.php

---
+++ uploads/index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="client-nojs">
+<head>
+<meta charset="UTF-8" />
+<title>File:TourAmmoSeekOption.png - AmmoSeek Help</title>
+<meta name="generator" content="MediaWiki 1.24.1" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="search" type="application/opensearchdescription+xml" href="http://help.ammoseek.com/opensearch_desc.php" title="AmmoSeek Help (en)" />
+<link rel="EditURI" type="application/rsd+xml" href="http://help.ammoseek.com/api.php?action=rsd" />
+<link rel="alternate" hreflang="x-default" href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png" />
+<link rel="alternate" type="application/atom+xml" title="AmmoSeek Help Atom feed" href="http://help.ammoseek.com/index.php?title=Special:RecentChanges&amp;feed=atom" />
+<link rel="stylesheet" href="http://help.ammoseek.com/load.php?debug=false&amp;lang=en&amp;modules=mediawiki.legacy.commonPrint%2Cshared%7Cmediawiki.skinning.interface%7Cmediawiki.ui.button%7Cskins.vector.styles&amp;only=styles&amp;skin=vector&amp;*" />
+<meta name="ResourceLoaderDynamicStyles" content="" />
+<style>a:lang(ar),a:lang(kk-arab),a:lang(mzn),a:lang(ps),a:lang(ur){text-decoration:none}
+/* cache key: ammoseek_help:resourceloader:filter:minify-css:7:a2f9dae30f6db6ed1f1e8252365dffd6 */</style>
+<script src="http://help.ammoseek.com/load.php?debug=false&amp;lang=en&amp;modules=startup&amp;only=scripts&amp;skin=vector&amp;*"></script>
+<script>if(window.mw){
+mw.config.set({"wgCanonicalNamespace":"File","wgCanonicalSpecialPageName":false,"wgNamespaceNumber":6,"wgPageName":"File:TourAmmoSeekOption.png","wgTitle":"TourAmmoSeekOption.png","wgCurRevisionId":602,"wgRevisionId":602,"wgArticleId":122,"wgIsArticle":true,"wgIsRedirect":false,"wgAction":"view","wgUserName":null,"wgUserGroups":["*"],"wgCategories":[],"wgBreakFrames":false,"wgPageContentLanguage":"en","wgPageContentModel":"wikitext","wgSeparatorTransformTable":["",""],"wgDigitTransformTable":["",""],"wgDefaultDateFormat":"dmy","wgMonthNames":["","January","February","March","April","May","June","July","August","September","October","November","December"],"wgMonthNamesShort":["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"wgRelevantPageName":"File:TourAmmoSeekOption.png","wgIsProbablyEditable":false,"wgRestrictionEdit":[],"wgRestrictionMove":[],"wgRestrictionUpload":[]});
+}</script><script>if(window.mw){
+mw.loader.implement("user.options",function($,jQuery){mw.user.options.set({"ccmeonemails":0,"cols":80,"date":"default","diffonly":0,"disablemail":0,"editfont":"default","editondblclick":0,"editsectiononrightclick":0,"enotifminoredits":0,"enotifrevealaddr":0,"enotifusertalkpages":1,"enotifwatchlistpages":1,"extendwatchlist":0,"fancysig":0,"forceeditsummary":0,"gender":"unknown","hideminor":0,"hidepatrolled":0,"imagesize":2,"math":1,"minordefault":0,"newpageshidepatrolled":0,"nickname":"","norollbackdiff":0,"numberheadings":0,"previewonfirst":0,"previewontop":1,"rcdays":7,"rclimit":50,"rows":25,"showhiddencats":0,"shownumberswatching":1,"showtoolbar":1,"skin":"vector","stubthreshold":0,"thumbsize":5,"underline":2,"uselivepreview":0,"usenewrc":0,"watchcreations":1,"watchdefault":1,"watchdeletion":0,"watchlistdays":3,"watchlisthideanons":0,"watchlisthidebots":0,"watchlisthideliu":0,"watchlisthideminor":0,"watchlisthideown":0,"watchlisthidepatrolled":0,"watchmoves":0,"watchrollback":0,
+"wllimit":250,"useeditwarning":1,"prefershttps":1,"language":"en","variant-gan":"gan","variant-iu":"iu","variant-kk":"kk","variant-ku":"ku","variant-shi":"shi","variant-sr":"sr","variant-tg":"tg","variant-uz":"uz","variant-zh":"zh","searchNs0":true,"searchNs1":false,"searchNs2":false,"searchNs3":false,"searchNs4":false,"searchNs5":false,"searchNs6":false,"searchNs7":false,"searchNs8":false,"searchNs9":false,"searchNs10":false,"searchNs11":false,"searchNs12":false,"searchNs13":false,"searchNs14":false,"searchNs15":false,"variant":"en"});},{},{});mw.loader.implement("user.tokens",function($,jQuery){mw.user.tokens.set({"editToken":"+\\","patrolToken":"+\\","watchToken":"+\\"});},{},{});
+/* cache key: ammoseek_help:resourceloader:filter:minify-js:7:2ffb09fddb30ef53a64cefb237597916 */
+}</script>
+<script>if(window.mw){
+mw.loader.load(["mediawiki.page.startup","mediawiki.legacy.wikibits","mediawiki.legacy.ajax","skins.vector.js"]);
+}</script>
+<!--[if lt IE 7]><style type="text/css">body{behavior:url("http://help.ammoseek.com/skins/Vector/csshover.min.htc")}</style><![endif]-->
+</head>
+<body class="mediawiki ltr sitedir-ltr ns-6 ns-subject page-File_TourAmmoSeekOption_png skin-vector action-view vector-animateLayout">
+		<div id="mw-page-base" class="noprint"></div>
+		<div id="mw-head-base" class="noprint"></div>
+		<div id="content" class="mw-body" role="main">
+			<a id="top"></a>
+
+						<h1 id="firstHeading" class="firstHeading" lang="en"><span dir="auto">File:TourAmmoSeekOption.png</span></h1>
+						<div id="bodyContent" class="mw-body-content">
+									<div id="siteSub">From AmmoSeek Help</div>
+								<div id="contentSub"></div>
+												<div id="jump-to-nav" class="mw-jump">
+					Jump to:					<a href="#mw-navigation">navigation</a>, 					<a href="#p-search">search</a>
+				</div>
+				<div id="mw-content-text"><ul id="filetoc"><li><a href="#file">File</a></li>
+<li><a href="#filehistory">File history</a></li>
+<li><a href="#filelinks">File usage</a></li>
+<li><a href="#metadata">Metadata</a></li></ul><div class="fullImageLink" id="file"><a href="http://help.ammoseek.com/images/a/a7/TourAmmoSeekOption.png"><img alt="File:TourAmmoSeekOption.png" src="http://help.ammoseek.com/images/a/a7/TourAmmoSeekOption.png" width="178" height="322" /></a><div class="mw-filepage-resolutioninfo">No higher resolution available.</div></div>
+<div class="fullMedia"><a href="http://help.ammoseek.com/images/a/a7/TourAmmoSeekOption.png" class="internal" title="TourAmmoSeekOption.png">TourAmmoSeekOption.png</a> &#8206;<span class="fileInfo">(178 × 322 pixels, file size: 12 KB, MIME type: image/png)</span>
+</div>
+<div id="mw-imagepage-content" lang="en" dir="ltr" class="mw-content-ltr">
+<!-- 
+NewPP limit report
+CPU time usage: 0.000 seconds
+Real time usage: 0.000 seconds
+Preprocessor visited node count: 0/1000000
+Preprocessor generated node count: 0/1000000
+Post‐expand include size: 0/2097152 bytes
+Template argument size: 0/2097152 bytes
+Highest expansion depth: 0/40
+Expensive parser function count: 0/100
+-->
+
+<!-- Saved in parser cache with key ammoseek_help:pcache:idhash:122-0!*!*!*!*!*!* and timestamp 20191023202901 and revision id 602
+ -->
+</div><h2 id="filehistory">File history</h2>
+<div id="mw-imagepage-section-filehistory">
+<p>Click on a date/time to view the file as it appeared at that time.
+</p>
+<table class="wikitable filehistory">
+<tr><td></td><th>Date/Time</th><th>Thumbnail</th><th>Dimensions</th><th>User</th><th>Comment</th></tr>
+<tr><td>current</td><td class='filehistory-selected' style='white-space: nowrap;'><a href="http://help.ammoseek.com/images/a/a7/TourAmmoSeekOption.png">15:32, 19 July 2019</a></td><td><a href="http://help.ammoseek.com/images/a/a7/TourAmmoSeekOption.png"><img alt="Thumbnail for version as of 15:32, 19 July 2019" src="http://help.ammoseek.com/images/thumb/a/a7/TourAmmoSeekOption.png/66px-TourAmmoSeekOption.png" width="66" height="120" /></a></td><td>178 × 322 <span style="white-space: nowrap;">(12 KB)</span></td><td><a href="http://help.ammoseek.com/index.php?title=User:Mike&amp;action=edit&amp;redlink=1" class="new mw-userlink" title="User:Mike (page does not exist)">Mike</a> <span style="white-space: nowrap;"> <span class="mw-usertoollinks">(<a href="http://help.ammoseek.com/index.php?title=User_talk:Mike&amp;action=edit&amp;redlink=1" class="new" title="User talk:Mike (page does not exist)">Talk</a> | <a href="http://help.ammoseek.com/index.php?title=Special:Contributions/Mike" title="Special:Contributions/Mike">contribs</a>)</span></span></td><td dir="ltr"></td></tr>
+</table>
+
+</div>
+<ul>
+<li id="mw-imagepage-upload-disallowed">You cannot overwrite this file.</li>
+</ul>
+<h2 id="filelinks">File usage</h2>
+<div id='mw-imagepage-section-linkstoimage'>
+<p>The following page links to this file:
+</p><ul class="mw-imagepage-linkstoimage">
+<li class="mw-imagepage-linkstoimage-ns0"><a href="http://help.ammoseek.com/index.php?title=AmmoSeek_Documentation" title="AmmoSeek Documentation">AmmoSeek Documentation</a></li>
+</ul>
+</div>
+<h2 id="metadata">Metadata</h2>
+<div class="mw-imagepage-section-metadata">This file contains additional information, probably added from the digital camera or scanner used to create or digitize it.
+If the file has been modified from its original state, some details may not fully reflect the modified file.<table id="mw_metadata" class="mw_metadata">
+<tr class="exif-xresolution collapsable">
+<th>Horizontal resolution</th>
+<td>28.35 dpc</td>
+</tr><tr class="exif-yresolution collapsable">
+<th>Vertical resolution</th>
+<td>28.35 dpc</td>
+</tr><tr class="exif-datetime collapsable">
+<th>File change date and time</th>
+<td>19:28, 19 July 2019</td>
+</tr></table>
+</div>
+</div>									<div class="printfooter">
+						Retrieved from "<a dir="ltr" href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;oldid=602">http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;oldid=602</a>"					</div>
+													<div id='catlinks' class='catlinks catlinks-allhidden'></div>												<div class="visualClear"></div>
+							</div>
+		</div>
+		<div id="mw-navigation">
+			<h2>Navigation menu</h2>
+
+			<div id="mw-head">
+									<div id="p-personal" role="navigation" class="" aria-labelledby="p-personal-label">
+						<h3 id="p-personal-label">Personal tools</h3>
+						<ul>
+							<li id="pt-login"><a href="http://help.ammoseek.com/index.php?title=Special:UserLogin&amp;returnto=File%3ATourAmmoSeekOption.png" title="You are encouraged to log in; however, it is not mandatory [o]" accesskey="o">Log in</a></li>						</ul>
+					</div>
+									<div id="left-navigation">
+										<div id="p-namespaces" role="navigation" class="vectorTabs" aria-labelledby="p-namespaces-label">
+						<h3 id="p-namespaces-label">Namespaces</h3>
+						<ul>
+															<li  id="ca-nstab-image" class="selected"><span><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png"  title="View the file page [c]" accesskey="c">File</a></span></li>
+															<li  id="ca-talk" class="new"><span><a href="http://help.ammoseek.com/index.php?title=File_talk:TourAmmoSeekOption.png&amp;action=edit&amp;redlink=1"  title="Discussion about the content page [t]" accesskey="t">Discussion</a></span></li>
+													</ul>
+					</div>
+										<div id="p-variants" role="navigation" class="vectorMenu emptyPortlet" aria-labelledby="p-variants-label">
+												<h3 id="p-variants-label"><span>Variants</span><a href="#"></a></h3>
+
+						<div class="menu">
+							<ul>
+															</ul>
+						</div>
+					</div>
+									</div>
+				<div id="right-navigation">
+										<div id="p-views" role="navigation" class="vectorTabs" aria-labelledby="p-views-label">
+						<h3 id="p-views-label">Views</h3>
+						<ul>
+															<li id="ca-view" class="selected"><span><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png" >Read</a></span></li>
+															<li id="ca-viewsource"><span><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;action=edit"  title="This page is protected.&#10;You can view its source [e]" accesskey="e">View source</a></span></li>
+															<li id="ca-history" class="collapsible"><span><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;action=history"  title="Past revisions of this page [h]" accesskey="h">View history</a></span></li>
+													</ul>
+					</div>
+										<div id="p-cactions" role="navigation" class="vectorMenu emptyPortlet" aria-labelledby="p-cactions-label">
+						<h3 id="p-cactions-label"><span>More</span><a href="#"></a></h3>
+
+						<div class="menu">
+							<ul>
+															</ul>
+						</div>
+					</div>
+										<div id="p-search" role="search">
+						<h3>
+							<label for="searchInput">Search</label>
+						</h3>
+
+						<form action="http://help.ammoseek.com/index.php" id="searchform">
+														<div id="simpleSearch">
+															<input type="search" name="search" placeholder="Search" title="Search AmmoSeek Help [f]" accesskey="f" id="searchInput" /><input type="hidden" value="Special:Search" name="title" /><input type="submit" name="fulltext" value="Search" title="Search the pages for this text" id="mw-searchButton" class="searchButton mw-fallbackSearchButton" /><input type="submit" name="go" value="Go" title="Go to a page with this exact name if exists" id="searchButton" class="searchButton" />								</div>
+						</form>
+					</div>
+									</div>
+			</div>
+			<div id="mw-panel">
+				<div id="p-logo" role="banner"><a style="background-image: url(http://help.ammoseek.com/resources/assets/as_square_144x144_transparent.png);" href="http://help.ammoseek.com/index.php?title=AmmoSeek_Help"  title="Visit the main page"></a></div>
+						<div class="portal" role="navigation" id='p-navigation' aria-labelledby='p-navigation-label'>
+			<h3 id='p-navigation-label'>Navigation</h3>
+
+			<div class="body">
+									<ul>
+													<li id="n-mainpage-description"><a href="http://help.ammoseek.com/index.php?title=AmmoSeek_Help" title="Visit the main page [z]" accesskey="z">Main page</a></li>
+													<li id="n-recentchanges"><a href="http://help.ammoseek.com/index.php?title=Special:RecentChanges" title="A list of recent changes in the wiki [r]" accesskey="r">Recent changes</a></li>
+													<li id="n-randompage"><a href="http://help.ammoseek.com/index.php?title=Special:Random" title="Load a random page [x]" accesskey="x">Random page</a></li>
+													<li id="n-help"><a href="https://www.mediawiki.org/wiki/Special:MyLanguage/Help:Contents" target="_blank" title="The place to find out">Help</a></li>
+											</ul>
+							</div>
+		</div>
+			<div class="portal" role="navigation" id='p-tb' aria-labelledby='p-tb-label'>
+			<h3 id='p-tb-label'>Tools</h3>
+
+			<div class="body">
+									<ul>
+													<li id="t-whatlinkshere"><a href="http://help.ammoseek.com/index.php?title=Special:WhatLinksHere/File:TourAmmoSeekOption.png" title="A list of all wiki pages that link here [j]" accesskey="j">What links here</a></li>
+													<li id="t-recentchangeslinked"><a href="http://help.ammoseek.com/index.php?title=Special:RecentChangesLinked/File:TourAmmoSeekOption.png" title="Recent changes in pages linked from this page [k]" accesskey="k">Related changes</a></li>
+													<li id="t-specialpages"><a href="http://help.ammoseek.com/index.php?title=Special:SpecialPages" title="A list of all special pages [q]" accesskey="q">Special pages</a></li>
+													<li id="t-print"><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;printable=yes" rel="alternate" title="Printable version of this page [p]" accesskey="p">Printable version</a></li>
+													<li id="t-permalink"><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;oldid=602" title="Permanent link to this revision of the page">Permanent link</a></li>
+													<li id="t-info"><a href="http://help.ammoseek.com/index.php?title=File:TourAmmoSeekOption.png&amp;action=info">Page information</a></li>
+											</ul>
+							</div>
+		</div>
+				</div>
+		</div>
+		<div id="footer" role="contentinfo">
+							<ul id="footer-info">
+											<li id="footer-info-lastmod"> This page was last modified on 19 July 2019, at 15:32.</li>
+											<li id="footer-info-viewcount">This page has been accessed 29 times.</li>
+									</ul>
+							<ul id="footer-places">
+											<li id="footer-places-privacy"><a href="http://help.ammoseek.com/index.php?title=AmmoSeek_Help:Privacy_policy" title="AmmoSeek Help:Privacy policy">Privacy policy</a></li>
+											<li id="footer-places-about"><a href="http://help.ammoseek.com/index.php?title=AmmoSeek_Help:About" title="AmmoSeek Help:About">About AmmoSeek Help</a></li>
+											<li id="footer-places-disclaimer"><a href="http://help.ammoseek.com/index.php?title=AmmoSeek_Help:General_disclaimer" title="AmmoSeek Help:General disclaimer">Disclaimers</a></li>
+									</ul>
+										<ul id="footer-icons" class="noprint">
+											<li id="footer-poweredbyico">
+															<a href="//www.mediawiki.org/"><img src="http://help.ammoseek.com/resources/assets/poweredby_mediawiki_88x31.png" alt="Powered by MediaWiki" width="88" height="31" /></a>
+													</li>
+									</ul>
+						<div style="clear:both"></div>
+		</div>
+		<script>/*<![CDATA[*/window.jQuery && jQuery.ready();/*]]>*/</script><script>if(window.mw){
+mw.loader.state({"site":"ready","user":"ready","user.groups":"ready"});
+}</script>
+<script>if(window.mw){
+mw.loader.load(["mediawiki.action.view.postEdit","mediawiki.action.view.metadata","mediawiki.user","mediawiki.hidpi","mediawiki.page.ready","mediawiki.searchSuggest"],null,true);
+}</script>
+<script>if(window.mw){
+mw.config.set({"wgBackendResponseTime":292});
+}</script>
+	</body>
+</html>
+	
